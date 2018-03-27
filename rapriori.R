134667fd45c6b5b06c22ccea876ac27283ee18f7
jevelezse <jevelezse@unal.edu.co>
2018-03-27 10:06:13 -0500

    Filtro de genes y reglas

--- rapriori.R
+++ rapriori.R
@@ -3,11 +3,11 @@ library("arulesViz") #visualización
 library("dplyr")
 library("tidyr")
 library("magrittr")
-genes <- read.csv('/home/jennifer/Escritorio/output.csv') 
+genes <- read.csv('/home/jennifer/Escritorio/genesok.csv') 
 
-genes %<>% filter(cluster %in% c('c1','c5','c3','c4','c2'))
-#genes %<>% filter(cluster %in% c('c1','c5'))
-genes %<>% filter(gen %in% c('BRCA1','BRCA2'))
+#genes %<>% filter(cluster %in% c('c1','c5','c3','c4','c2'))
+#genes %<>% filter(cluster %in% c('c1','c3','c5'))
+genes %<>% filter(gen %in% c('BRCA2','BRCA1','ATM'))
 
 #genes %<>% select(-cluster)
 
@@ -16,9 +16,16 @@ genes %<>% filter(gen %in% c('BRCA1','BRCA2'))
 #genes$edad <- discretize(genes$edad)
 #genes$sexo <- discretize(genes$sexo)
 g <- as(genes, "transactions")
-rules <- apriori(g, parameter = list(supp = 0.01, conf = 0.6, target = "rules"))
+rules <- apriori(g, parameter = list(supp = 0.1, conf = 0.6, target = "rules"))
+rules <-sort(rules, by ="lift")
 #gato <- as(rules,"data.frame")
 gatos <- data.frame(lhs = labels(lhs(rules)), rhs = labels(rhs(rules)), rules@quality) 
+gatos$id <- rownames(gatos)
+gatos %>% filter((grepl("BRCA1",lhs)|grepl("BRCA2",lhs)|grepl("ATM",lhs))) %>% select(id)-> rules_filtered
+rules_filtered <- as.vector(rules_filtered) #Filtro de reglas por gen.
+
+plot(rules[rules_filtered[1:30,1]], method="graph", control=list(type="items"))
+
 summary(rules)
 inspect(rules)
 
@@ -26,3 +33,9 @@ plot(rules[1:5], method="graph", control=list(type="items"))
 plot(rules[1:10], method="graph", control=list(type="items"))
 plot(rules[10:15], method="graph", control=list(type="items"))
 plot(rules, method="graph", control=list(type="items"))
+
+inspect(head(rules, by = "lift"))
+## filter spurious rules 
+inspect(rules, by = "lift")
+plot(rules[rules_filtered[1:30,1]], method="graph", control=list(type="items"))
+
